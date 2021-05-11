19d66e5002aa98bbfb65439bf772ac95824195bc
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-05-11 13:03:41 +0200

    Add Universes component

---
+++ src/Components/Universes/Universes.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import Grid from '@material-ui/core/Grid';
+import axios from '../../Api/axios'
+
+export default function Universes({ fetchUrl }) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
